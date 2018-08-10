d952d8a4d26f5cb4cbf3cba03078bb6bfe414d6d
Matan <matanmazor@outlook.com>
2018-08-10 12:17:53 +0100

    nii instead of nii.gz, to be compatible with SPM12

--- FIL2BIDS.m
+++ FIL2BIDS.m
@@ -24,7 +24,7 @@ for i=1:length(sequence_idx)
 end
 
 %% 3. Save as niftis and jsons
-dicm2nii(fullfile(source_dir, sequence_name),fullfile(target_dir, sequence_name))
+dicm2nii(fullfile(source_dir, sequence_name),fullfile(target_dir, sequence_name),'nii')
 rmdir(fullfile(source_dir, sequence_name),'s');
 
 %% 4. Validate that everything makes sense, and then change file names
@@ -33,45 +33,45 @@ switch sequence_name
         %here I assume that the anatomical image should be mprage
         if ~exist(fullfile(target_dir, sequence_name, 'MPRAGE_64ch_Head.json'), 'file')
             error('Couldn''t find MPRAGE_64ch_Head.json')
-        elseif ~exist(fullfile(target_dir, sequence_name, 'MPRAGE_64ch_Head.nii.gz'), 'file')
+        elseif ~exist(fullfile(target_dir, sequence_name, 'MPRAGE_64ch_Head.nii'), 'file')
             error('Couldn''t find MPRAGE_64ch_Head.nii.gz')
         end
         
         movefile(fullfile(target_dir, sequence_name, 'MPRAGE_64ch_Head.json'),...
             fullfile(target_dir, sequence_name, strcat('sub-',subj_id,'_T1w.json')));
         
-        movefile(fullfile(target_dir, sequence_name, 'MPRAGE_64ch_Head.nii.gz'),...
-            fullfile(target_dir, sequence_name, strcat('sub-',subj_id,'_T1w.nii.gz')));
+        movefile(fullfile(target_dir, sequence_name, 'MPRAGE_64ch_Head.nii'),...
+            fullfile(target_dir, sequence_name, strcat('sub-',subj_id,'_T1w.nii')));
         
     case 'fmap'
         %here I assume that the fieldmap image is of a certain type
         if ~exist(fullfile(target_dir, sequence_name, 'gre_field_mapping_1acq_rl.json'), 'file')
             error('Couldn''t find gre_field_mapping_1acq_rl.json')
-        elseif ~exist(fullfile(target_dir, sequence_name, 'gre_field_mapping_1acq_rl.nii.gz'), 'file')
-            error('Couldn''t find gre_field_mapping_1acq_rl.nii.gz')
+        elseif ~exist(fullfile(target_dir, sequence_name, 'gre_field_mapping_1acq_rl.nii'), 'file')
+            error('Couldn''t find gre_field_mapping_1acq_rl.nii')
         elseif ~exist(fullfile(target_dir, sequence_name, 'gre_field_mapping_1acq_rl_phase.json'), 'file')
             error('Couldn''t find gre_field_mapping_1acq_rl_phase.json')
-        elseif ~exist(fullfile(target_dir, sequence_name, 'gre_field_mapping_1acq_rl_phase.nii.gz'), 'file')
-            error('Couldn''t find gre_field_mapping_1acq_rl_phase.nii.gz')
+        elseif ~exist(fullfile(target_dir, sequence_name, 'gre_field_mapping_1acq_rl_phase.nii'), 'file')
+            error('Couldn''t find gre_field_mapping_1acq_rl_phase.nii')
         end
         
         movefile(fullfile(target_dir, sequence_name, 'gre_field_mapping_1acq_rl.json'),...
             fullfile(target_dir, sequence_name, strcat('sub-',subj_id,'_magnitude1.json')));
         
-        movefile(fullfile(target_dir, sequence_name, 'gre_field_mapping_1acq_rl.nii.gz'),...
-            fullfile(target_dir, sequence_name, strcat('sub-',subj_id,'_magnitude1.nii.gz')));
+        movefile(fullfile(target_dir, sequence_name, 'gre_field_mapping_1acq_rl.nii'),...
+            fullfile(target_dir, sequence_name, strcat('sub-',subj_id,'_magnitude1.nii')));
         
         movefile(fullfile(target_dir, sequence_name, 'gre_field_mapping_1acq_rl_phase.json'),...
             fullfile(target_dir, sequence_name, strcat('sub-',subj_id,'_phasediff.json')));
         
-        movefile(fullfile(target_dir, sequence_name, 'gre_field_mapping_1acq_rl_phase.nii.gz'),...
-            fullfile(target_dir, sequence_name, strcat('sub-',subj_id,'_phasediff.nii.gz')));
+        movefile(fullfile(target_dir, sequence_name, 'gre_field_mapping_1acq_rl_phase.nii'),...
+            fullfile(target_dir, sequence_name, strcat('sub-',subj_id,'_phasediff.nii')));
     case 'func'
         %here I assume that the _events.tsv files have already been placed in the
         %folder and that they are ordered like the functional scannings
         tsv_files = dir(fullfile(target_dir,'func','*_events.tsv'));
         json_files = dir(fullfile(target_dir,'func','*.json'));
-        image_files = dir(fullfile(target_dir,'func','*.nii.gz'));
+        image_files = dir(fullfile(target_dir,'func','*.nii'));
         if size(tsv_files) ~= size(json_files) 
             error('different number of event files and functional scans')
         end
@@ -82,7 +82,7 @@ switch sequence_name
             movefile(fullfile(target_dir, sequence_name, json_files(i).name),...
                        fullfile(target_dir, sequence_name, strcat(run_name,'_bold.json'))); 
             movefile(fullfile(target_dir, sequence_name, image_files(i).name),...
-                       fullfile(target_dir, sequence_name, strcat(run_name,'_bold.nii.gz'))); 
+                       fullfile(target_dir, sequence_name, strcat(run_name,'_bold.nii'))); 
         end
    
 end
